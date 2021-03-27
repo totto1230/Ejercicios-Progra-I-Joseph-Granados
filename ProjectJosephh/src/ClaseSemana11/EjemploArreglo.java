90e4ed6d5d9e07c00131ae9113e8604488c1d821
joseph <joseph@DESKTOP-1FURMSB>
2021-03-26 21:47:10 -0600

    Joseph

--- ProjectJosephh/src/ClaseSemana11/EjemploArreglo.java
+++ ProjectJosephh/src/ClaseSemana11/EjemploArreglo.java
@@ -1,22 +1,17 @@
 package ClaseSemana11;
 
-
 public class EjemploArreglo {
 
     public static void main(String[] args) {
-        int[] a= {1,2,3};
-        int[]aa= new int[4];
-        
-        aa[1]=1;
-        System.out.println(aa[0]);
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        int[] a = {1, 2, 3};
+        int[] aa = new int[100];
+
+        for (int iw = 0; iw < aa.length; iw++) {
+            for (int i = 0; i < aa.length; i++) {
+                aa[i] = 1;
+
+            }
+            System.out.println(aa[iw]);
+        }
     }
 }
