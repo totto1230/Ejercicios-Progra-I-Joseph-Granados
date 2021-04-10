cad37383d6bec0e57e1343a7bee402b7f46f0cf0
joseph <joseph@DESKTOP-1FURMSB>
2021-04-09 22:20:20 -0600

    Joseph

---
+++ ProjectJosephh/src/problema1proyecto/Tester.java
@@ -0,0 +1,15 @@
+package problema1proyecto;
+
+
+
+public class Tester {
+
+    public static void main(String[] args) {
+
+        /* Scanner s = new Scanner (System.in);
+        double w=s.nextDouble();*/
+        Conversiones c = new Conversiones();
+        c.Convertir();
+    }
+
+}
