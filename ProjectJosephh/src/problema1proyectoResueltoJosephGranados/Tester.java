15cdb986e3efa9225195043a71fe128956abf8b6
joseph <joseph@DESKTOP-1FURMSB>
2021-04-11 17:52:01 -0600

    COMMIT FINAL, FUNCIONA AS EXPECTED

---
+++ ProjectJosephh/src/problema1proyectoResueltoJosephGranados/Tester.java
@@ -0,0 +1,15 @@
+package problema1proyectoResueltoJosephGranados;
+
+
+
+public class Tester {
+
+    public static void main(String[] args) {
+
+        /* Scanner s = new Scanner (System.in);
+        double w=s.nextDouble();*/
+        Conversiones c = new Conversiones();
+        c.Convertir();
+    }
+
+}
