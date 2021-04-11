799c7fb085dbf9a7eb5135af6aac0eada065363e
joseph <joseph@DESKTOP-1FURMSB>
2021-04-11 13:30:39 -0600

    Joseph

---
+++ ProjectJosephh/src/problema3proyecto/Rectangulo.java
@@ -0,0 +1,25 @@
+
+package problema3proyecto;
+
+import java.awt.Graphics;
+
+public class Rectangulo {
+  public void paint (Graphics g){
+     // super.paint(g);
+      
+      //LINEAS
+      g.drawLine(25,90,60,50);
+      
+      
+      
+      
+      
+      
+      
+      
+  } 
+    
+    
+    
+    
+}
---
+++ ProjectJosephh/src/problema3proyecto/Tester.java
@@ -0,0 +1,14 @@
+package problema3proyecto;
+
+public class Tester {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
