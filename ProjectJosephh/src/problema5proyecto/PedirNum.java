501226a46abc1535561b9dd75f245e955b9658ef
joseph <joseph@DESKTOP-1FURMSB>
2021-04-11 16:26:14 -0600

    SE PIDE EL NUMERO DE NUMEROS QUE SE QUIERE METER EN EL ARREGLO             es decir el famoso "array.length"1

--- ProjectJosephh/src/problema5proyecto/PedirNum.java
+++ ProjectJosephh/src/problema5proyecto/PedirNum.java
@@ -6,21 +6,39 @@ import java.util.Scanner;
 public class PedirNum {
 
     private int[] cont1 ;
+    private int[] cont2 ;
     private int espacios=0;
+    private int espacios2=0;
 
     public void PedirNumm() {
-        Scanner s = new Scanner(System.in);
-        System.out.println("Numero de ints quiere meter");
-        this.espacios=s.nextInt();
         
-        for (int i = 0; i<100; i++) {
-           
-            System.out.println("Ingrese el dato");
-           
-            //cont1[i] =  new int [s.nextInt();
-            System.out.println(cont1[i]);
+        /* SE PIDE EL NUMERO DE NUMEROS QUE SE QUIERE METER EN EL ARREGLO
+            es decir el famoso "array.length"
+        */
+        
+        Scanner s = new Scanner(System.in);
+        System.out.println("Numero de ints quiere meter en el primer conjunto");
+        this.espacios=s.nextInt();
+        cont1= new int [espacios];
+        System.out.println("Numero de ints quiere meter en el segundo conjunto");
+        this.espacios2=s.nextInt();
+        cont2= new int[espacios2];
+        
+        for (int i = 0; i<espacios; i++) {
+            System.out.println("Digite el int number: "+ i+1);
+            int a=s.nextInt();
+            cont1[i]=a;
+            
         }
 
+    }
+
+    public void setCont1(int[] cont1) {
+        this.cont1 = cont1;
+    }
+
+    public void setEspacios(int espacios) {
+        this.espacios = espacios;
     }
 
     public int[] getCont1() {
