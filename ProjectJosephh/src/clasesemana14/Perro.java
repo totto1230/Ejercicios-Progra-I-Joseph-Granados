861968f736489659645d69263ee6ea1b1f15b456
joseph <joseph@DESKTOP-1FURMSB>
2021-04-23 18:16:04 -0600

    Entendiendo metodos Set, Getter and Constructor

--- ProjectJosephh/src/clasesemana14/Perro.java
+++ ProjectJosephh/src/clasesemana14/Perro.java
@@ -2,25 +2,64 @@
 package clasesemana14;
 
 public class Perro {
+    public String nombre;
+    public static int contador;
+    public String color;
+    
+    
+    public Perro() {
+     
+    }
+
+    public Perro(String nombre, String color) {
+        this.nombre = nombre;
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Perro{" + "nombre=" + nombre + ", color=" + color + '}';
+    }
+
+    public static void setContador(int contador) {
+        Perro.contador = contador;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
     
     
     
+    //Metodo Constructor para construir el objeto, es decir, le da el valor al objeto
+
+  
+     public Perro(String nombre) {
+        this.nombre = nombre;
+    }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+   
+    //Para poner el nombre, para enviar el nombre
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    //Obtener el nombre, para jalar el nombre
+    public String getNombre() {
+        return nombre;
+    }
+
+    //Obtener el nombre, para jalar el nombre
+    public static int getContador() {
+        return contador;
+    }
+
+   
+
     
     
     
--- ProjectJosephh/src/clasesemana14/Tester.java
+++ ProjectJosephh/src/clasesemana14/Tester.java
@@ -1,20 +1,16 @@
-
 package clasesemana14;
 
-
 public class Tester {
-    
+
     public static void main(String[] args) {
-        Perro p1= new Perro();
+        Perro p1 = new Perro();
+        p1.setNombre("Name1"); //Ponerle un nombre, es decir setearle un valor quemado
+        p1.getNombre();
         
         
+        Perro p2 = new Perro("NAme2", "Verde");
+        
+       p2.toString();
     }
-    
-    
-    
-    
-    
-    
-    
-    
+
 }
